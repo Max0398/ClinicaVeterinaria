56169f1a20033b59adfa48e44ddede8afa9163bd
Maximo <Guzmanmax1998@gmail.com>
2024-06-28 11:50:23 -0600

    Cambios Colores Menu

---
+++ wwwroot/js/menu1.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+    // Añadir transiciones suaves al abrir y cerrar los dropdowns
+    $('.dropdown').on('show.bs.dropdown', function () {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown(150);
+    });
+
+    // Solución para mantener abierto el menú desplegable al hacer clic en un enlace dentro del menú
+    $('.dropdown').on('click', function (e) {
+        if ($(this).hasClass('show')) {
+            $(this).find('.dropdown-menu').first().stop(true, true).slideUp(150);
+        } else {
+            $(this).find('.dropdown-menu').first().stop(true, true).slideDown(150);
+        }
+        $(this).toggleClass('show');
+    });
+
+    // Añadir clase activa al elemento de menú seleccionado
+    var currentLocation = location.pathname;
+    $('.navbar-nav a').each(function () {
+        var link = $(this).attr('href');
+        if (currentLocation.indexOf(link) !== -1) {
+            $(this).closest('li').addClass('active');
+            if ($(this).closest('.dropdown-menu').length) {
+                $(this).closest('.dropdown').addClass('active');
+                $(this).closest('.dropdown').addClass('show'); // Mostrar el menú desplegable activo
+            }
+        }
+    });
+});
---
+++ wwwroot/js/menuPrincipal.js
@@ -0,0 +1,23 @@
+﻿function toggleLoginMenu() {
+    var menu = document.getElementById('loginMenu');
+    menu.classList.toggle('show');
+}
+
+// Cerrar el menú si se hace clic fuera de él
+window.onclick = function (event) {
+    if (!event.target.matches('.login-icon') && !event.target.closest('.login-menu')) {
+        var dropdowns = document.getElementsByClassName("login-menu");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
+
+// Función para ocultar el menú después de iniciar sesión
+function hideLoginMenu() {
+    var menu = document.getElementById('loginMenu');
+    menu.classList.remove('show');
+}
